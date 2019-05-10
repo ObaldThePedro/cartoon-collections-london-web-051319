bcd70d43c01de5be58e8e7e50ee3b555f3d9b3c3
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-05-10 21:26:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each { |dwarf|
+    puts #{dwarf} }
+  # code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet code an argument here(array)
   # Your code here
+  array.collect { |planet|
+    planet.capitalize + "!"
+    puts planet }
+    end
 end
 
 def long_planeteer_calls# code an argument here
@@ -14,3 +21,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+ 
